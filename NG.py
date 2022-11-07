bdc888e49e749e8afe6a3b04d3bfb1706955161f
AKASH M <87299311+gamkers@users.noreply.github.com>
2022-11-07 19:43:35 +0530

    Update NG.py

--- NG.py
+++ NG.py
@@ -240,7 +240,7 @@ def webscrape_News(cat,n):
 def display(data):
     voice = []
     for i in range(5):
-        data1 = data[i][1]
+        data1 = data[i][1].split(":")
         voice.append(f"news number{str(i + 1)}," + data1[0] + '.')
     audio_bytes = speak('.'.join(map(str, voice)))
     st.audio(audio_bytes, format='audio/ogg')
