476507c41f43015d46de4d205ba40505bf50cfd4
AKASH M <87299311+gamkers@users.noreply.github.com>
2022-12-14 10:17:07 +0530

    Update NG.py

--- NG.py
+++ NG.py
@@ -40,8 +40,8 @@ def ppt(s):
             
             
             
-def torrent_download(search+" course"):
-    url = f"https://ww4.1337x.buzz/srch?search={search}"
+def torrent_download(search):
+    url = f"https://ww4.1337x.buzz/srch?search={search} course"
     r = requests.get(url)
     data = BeautifulSoup(r.text, "html.parser")
     links=data.find_all('a', style="font-family:tahoma;font-weight:bold;")
