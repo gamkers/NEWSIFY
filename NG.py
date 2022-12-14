ac6cb9e0daaaeb8630fa93b2621846b35c7f363b
AKASH M <87299311+gamkers@users.noreply.github.com>
2022-12-14 10:36:37 +0530

    Update NG.py

--- NG.py
+++ NG.py
@@ -270,12 +270,10 @@ if selected2 == 'Home':
 
 
 elif selected2 == 'File Search':
-
-     def local_css(file_name):
-            with open(file_name) as f:
-                st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-
-
+    def local_css(file_name):
+        with open(file_name) as f:
+            st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+                
     def remote_css(url):
         st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)
 
