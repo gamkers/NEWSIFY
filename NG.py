5143429d162f705ba9573a5c0dca0eeb7d6b6ce6
AKASH <87299311+gamkers@users.noreply.github.com>
2023-07-06 10:55:30 +0530

    Update NG.py

--- NG.py
+++ NG.py
@@ -133,7 +133,8 @@ def webscrape_MainNews(type):
     data_dict = [dict(zip(["headlines", "news", "authors", "Date", "country", "category", "images"], item)) for item in zip(headlines, news, authors, Date, country, catogory, images)]
     deta = Deta(st.secrets["data_key"])
     db = deta.Base("News")
-    db.put({type:data_dict})
+    for i in data_dict;
+        db.put(i)
     return data
 
 def webscrape_News(cat,n):
@@ -183,7 +184,8 @@ def webscrape_News(cat,n):
     data_dict = [dict(zip(["headlines", "news", "authors", "Date", "country", "category", "images"], item)) for item in zip(headlines, news, authors, Date, country, catogory, images)]
     deta = Deta(st.secrets["data_key"])
     db = deta.Base("News")
-    db.put({cat:data_dict})
+    for i in data_dict;
+        db.put(i)
     return data
 
 
